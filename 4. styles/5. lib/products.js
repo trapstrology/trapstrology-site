b49feb123dd79b1d3f5023829b504cba871b38fb
trapstrology <jonesclipps@gmail.com>
2025-04-21 10:35:07 -0500

    Create products.js

---
+++ 4. styles/5. lib/products.js
@@ -0,0 +1,79 @@
+export const products = [
+  {
+    id: 'full-spectrum-reading',
+    name: 'Full Spectrum Reading',
+    description:
+      'Unlock your cosmic blueprint with a fusion of astrology, numerology, and ancient remedies.',
+    price: 333.33
+  },
+  {
+    id: 'complete-self-discovery',
+    name: 'Complete Self‑Discovery',
+    description:
+      'Astrology can be a powerful tool for healing for business owners. By understanding your own chart.',
+    price: 155.55
+  },
+  {
+    id: 'big-3-analysis',
+    name: 'Big 3 Analysis',
+    description:
+      'An in‑depth analysis of your Rising, Sun, and Moon natal placements.',
+    price: 44.44
+  },
+  {
+    id: 'wheres-my-next-dollar',
+    name: "Where's My Next dollar?",
+    description:
+      'Money is becoming harder to obtain as the economic crisis continues. This will provide direction.',
+    price: 88.88
+  },
+  {
+    id: 'notoriety-fame-reading',
+    name: 'Notoriety & Fame Reading',
+    description:
+      "Do you believe you're destined to be well‑known? Let's take a look at your Natal Chart.",
+    price: 55.55
+  },
+  {
+    id: '10-heavenly-bodies',
+    name: '10 Heavenly Bodies',
+    description:
+      'Sun, Moon, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto',
+    price: 75.28
+  },
+  {
+    id: 'asteroids-hidden-talents',
+    name: 'Asteroids of Hidden Talents',
+    description:
+      'Discover your hidden talents with an analysis of 9 asteroids of the Muses and degree placements.',
+    price: 44.88
+  },
+  {
+    id: 'career-direction',
+    name: 'Career Direction',
+    description:
+      "Still haven't figured out what you wanna be when you grow up? Your Natal Chart can help with that.",
+    price: 66.66
+  },
+  {
+    id: 'relationship-synastry',
+    name: 'Complete Relationship Synastry Reading',
+    description:
+      'Comparison chart between you and your partner to analyze how their astrological energy is connected',
+    price: 188.88
+  },
+  {
+    id: '12-house-analysis',
+    name: '12‑House Analysis',
+    description:
+      'Receive a detailed analysis of all 12 houses and how they affect your habits and daily life.',
+    price: 50.88
+  },
+  {
+    id: 'child-chart-analysis',
+    name: 'Child Chart Analysis',
+    description:
+      "Learn and understand your child's love language, communication style, strengths, and weaknesses.",
+    price: 77.77
+  }
+];
